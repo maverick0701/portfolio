af02031f9b1040dec91f6a572b583a8bae2ccb19
madhav <madhavanandcp070@gmail.com>
2020-11-13 19:56:42 +0530

    front page almost done

--- assets/js/index.js
+++ assets/js/index.js
@@ -101,4 +101,120 @@ console.log(div);
 
 
 
-$(".progress-ring").loadingRing();
+
+
+
+
+var paperMenu = {
+	$window: $('#paper-window'),
+	$paperFront: $('#paper-front'),
+	$hamburger: $('.hamburger'),
+	offset: 1800,
+	pageHeight: $('#paper-front').outerHeight(),
+	
+	open: function() {
+		this.$window.addClass('tilt');
+		this.$hamburger.off('click');
+		$('#container, .hamburger').on('click', this.close.bind(this));
+		this.hamburgerFix(true);
+		console.log('opening...');
+	},
+	close: function() {
+		this.$window.removeClass('tilt'); 
+		$('#container, .hamburger').off('click');
+		this.$hamburger.on('click', this.open.bind(this));
+		this.hamburgerFix(false);
+		console.log('closing...');
+	},
+	updateTransformOrigin: function() {
+		scrollTop = this.$window.scrollTop();
+		equation = (scrollTop + this.offset) / this.pageHeight * 100;
+		this.$paperFront.css('transform-origin', 'center ' + equation + '%');
+	},
+	//hamburger icon fix to keep its position
+	hamburgerFix: function(opening) {
+			if(opening) {
+				$('.hamburger').css({
+					position: 'absolute',
+					top: this.$window.scrollTop() + 30 + 'px'
+				});
+			} else {
+				setTimeout(function() {
+					$('.hamburger').css({
+						position: 'fixed',
+						top: '30px'
+					});
+				}, 300);
+			}
+		},
+	bindEvents: function() {
+		this.$hamburger.on('click', this.open.bind(this));
+		$('.close').on('click', this.close.bind(this));
+		this.$window.on('scroll', this.updateTransformOrigin.bind(this));
+	},
+	init: function() {
+		this.bindEvents();
+		this.updateTransformOrigin();
+	},
+};
+
+paperMenu.init();
+
+
+$(".animated-progress span").each(function () {
+    $(this).animate(
+      {
+        width: $(this).attr("data-progress") + "%",
+      },
+      1000
+    );
+    $(this).text($(this).attr("data-progress") + "%");
+  });
+
+
+  var words = ['Hi i am Madhav', 'I  like to code', 'while(!(success=try())','Changing the world in my own small ways'],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 15,
+    speed = 70;
+var wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    }
+    else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      }
+      else {
+        offset--;
+      }
+    }
+    $('.word').text(part);
+  },speed);
+};
+
+$(document).ready(function () {
+  wordflick();
+});
